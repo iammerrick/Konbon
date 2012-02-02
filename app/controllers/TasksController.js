b489627141aa3d5417f44589d22ab0d56e6f4348
Kenneth Donaldson <kc@akuin.com>
2012-02-01 23:43:07 -0700

    started on tasks stuff

--- app/controllers/TasksController.js
+++ app/controllers/TasksController.js
@@ -1,7 +1,14 @@
-module.exports = require(app.set('controllers') + '/ApplicationController').extend()
+module.exports = require(app.set('controllers') + '/ApplicationController').extend({})
   .methods({
     index: function () {
-      this.json({"tasks" : [ {"description" : "Write a twitter connector", "started" : "Started January 2"}]});
-    }
-  })
+	    this.json({"tasks" : [ {"description" : "Write a twitter connector", "started" : "Started January 2"}]});
 
+	  	var config = require('../config/db');
+	  	var model = require('../models/Task');
+
+	  	var mytask = new model({"url": config.url});
+	  	
+		//just testing
+	    mytask.createDB('testing');
+	}
+})
---
+++ app/models/Tasks.js
@@ -0,0 +1,1 @@
+module.exports = BaseModel.extend(function(options){});
